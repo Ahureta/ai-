9d6d379c2235f1d0cdcb21cee28d152253e01d9c
雪 <242847242@qq.com>
2026-08-05 15:06:27 +0800

    下午第二

--- 8_5/8_5/Program.cs
+++ 8_5/8_5/Program.cs
@@ -52,6 +52,87 @@
             //{
             //    Console.WriteLine("不是闰年");
             //}
+
+            //输出星期几
+            //int n = int.Parse(Console.ReadLine());
+            //switch (n)
+            //{
+            //    case 1:
+            //        Console.WriteLine("星期一");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("星期二");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("星期三");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("星期四");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("星期五");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("星期六");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("星期日");
+            //        break;
+            //    default:
+            //        Console.WriteLine("输入有误");
+            //        break;
+            //}
+
+
+            // 输入分数 1~100
+            // 判断等级输出
+            // 分数90~100  输出A  ===> 分数的十位9 / 10
+            // 分数80~90   输出B  ===> 分数的十位8
+            // 分数70~80   输出C  ===> 分数的十位7
+            // 分数60~70   输出D  ===> 分数的十位6
+            // 分数1~60    输出F  ===> 分数的十位0/1/2/3/4/5
+
+            //int score = int.Parse(Console.ReadLine());
+            //int n2 = score / 10;
+            //switch (n2)
+            //{
+            //    case 0:
+            //        Console.WriteLine("F");
+            //        break;
+            //    case 1:
+            //        Console.WriteLine("F");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("F");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("F");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("F");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("F");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("D");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("C");
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("B");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("A");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("A");
+            //        break;
+            //    default:
+            //        Console.WriteLine("输入有误");
+            //        break;
+            //}
         }
     }
 }
