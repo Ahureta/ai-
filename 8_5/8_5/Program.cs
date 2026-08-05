a3f351ff199fb81bf5304ef8367ae2f89e6f31a3
雪 <242847242@qq.com>
2026-08-05 11:18:09 +0800

    8_5上午写作

---
+++ 8_5/8_5/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _8_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //小明择偶标准：要么资产在300w以上，要么颜值大于9.5。输入小红的资产和颜值
+            //double yuan = double.Parse(Console.ReadLine());
+            //Console.WriteLine(yuan);
+            //double faceValue = double.Parse(Console.ReadLine());
+            //Console.WriteLine(faceValue);
+            //bool isSuitable = yuan > 300 || faceValue > 9.5;
+            //Console.WriteLine(isSuitable);
+
+            //小红想做车模，车模条件年龄16~22
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine(age);
+            //bool isCarModel = age >= 16 && age <= 22;
+            //Console.WriteLine(isCarModel);
+
+            //根据输入的成绩判断是不及格
+            //(小于60),及格(大于60小于80), 良好(大于80小于90),优秀(大于90小于100)
+            //int score = int.Parse(Console.ReadLine());
+            //if (score < 60)
+            //{
+            //    Console.WriteLine("不及格");
+            //}
+            //else if (score >= 60 && score < 80)
+            //{
+            //    Console.WriteLine("及格");
+            //}
+            //else if (score >= 80 && score < 90)
+            //{
+            //    Console.WriteLine("良好");
+            //}
+            //else if (score >= 90 && score <= 100)
+            //{
+            //    Console.WriteLine("优秀");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("输入成绩有误");
+            //}
+
+            ////输入年份，判断是否是闰年(普通闰年：能被4整除但不能被100整除/世纪闰年：可以被400整除)
+            //int year = int.Parse(Console.ReadLine());
+            //if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            //{
+            //    Console.WriteLine("是闰年");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("不是闰年");
+            //}
+
+
+        }
+    }
+}
