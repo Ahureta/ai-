833e4c5ef666d985fa28f53693b32ea4eba8a86e
雪 <242847242@qq.com>
2026-08-07 17:21:16 +0800

    作业

--- 8_7/8_7/Program.cs
+++ 8_7/8_7/Program.cs
@@ -114,31 +114,142 @@ namespace _8_7
 
 
 
-            Console.WriteLine("输入price或者stock以及ASC(1)或者DSC(0)排序");    //LINQ.OrderByDescending()
-            String s = Console.ReadLine();
-            String orderInput = Console.ReadLine();
-            bool isAsc;
-            if (orderInput == "1" || orderInput.Equals("asc", StringComparison.OrdinalIgnoreCase))
-            {
-                isAsc = true;
-            }
-            else if (orderInput == "0" || orderInput.Equals("dsc", StringComparison.OrdinalIgnoreCase))
-            {
-                isAsc = false;
-            }
-            else if (!bool.TryParse(orderInput, out isAsc)) // 兼容true/false输入
-            {
-                Console.WriteLine("排序方式输入错误！请输入1/0/ASC/DSC");
-                return;
-            }
-            var result = isAsc
-                ? goodsList.OrderBy(g => g[s]).ToList()
-                : goodsList.OrderByDescending(g => g[s]).ToList();
+            //Console.WriteLine("输入price或者stock以及ASC(1)或者DSC(0)排序");    //LINQ.OrderByDescending()
+            //String s = Console.ReadLine();
+            //String orderInput = Console.ReadLine();
+            //bool isAsc;
+            //if (orderInput == "1" || orderInput.Equals("asc", StringComparison.OrdinalIgnoreCase))
+            //{
+            //    isAsc = true;
+            //}
+            //else if (orderInput == "0" || orderInput.Equals("dsc", StringComparison.OrdinalIgnoreCase))
+            //{
+            //    isAsc = false;
+            //}
+            //else if (!bool.TryParse(orderInput, out isAsc)) // 兼容true/false输入
+            //{
+            //    Console.WriteLine("排序方式输入错误！请输入1/0/ASC/DSC");
+            //    return;
+            //}
+            //var result = isAsc
+            //    ? goodsList.OrderBy(g => g[s]).ToList()
+            //    : goodsList.OrderByDescending(g => g[s]).ToList();
 
-            foreach (var goods in result)
+            //foreach (var goods in result)
+            //{
+            //    Console.WriteLine($"商品名称: {goods["name"]}, 价格: {goods["price"]}, 库存: {goods["stock"]}");
+            //}
+
+
+
+            List<Dictionary<string, dynamic>> singerList = new List<Dictionary<string, dynamic>>
             {
-                Console.WriteLine($"商品名称: {goods["name"]}, 价格: {goods["price"]}, 库存: {goods["stock"]}");
-            }
+                new Dictionary<string, dynamic>
+                {
+                    {"singerId", 1001},
+                    {"singerName", "周杰伦"},
+                    {"genre", "流行"}
+                },
+                new Dictionary<string, dynamic>
+                {
+                    {"singerId", 1002},
+                    {"singerName", "林俊杰"},
+                    {"genre", "华语流行"}
+                },
+                new Dictionary<string, dynamic>
+                {
+                    {"singerId", 1003},
+                    {"singerName", "邓紫棋"},
+                    {"genre", "流行、摇滚"}
+                },
+                new Dictionary<string, dynamic>
+                {
+                    {"singerId", 1004},
+                    {"singerName", "薛之谦"},
+                    {"genre", "抒情流行"}
+                },
+                new Dictionary<string, dynamic>
+                {
+                    {"singerId", 1005},
+                    {"singerName", "毛不易"},
+                    {"genre", "民谣流行"}
+                }
+            };
+
+            List<Dictionary<string, dynamic>> songList = new List<Dictionary<string, dynamic>>
+            {
+                new Dictionary<string, dynamic>
+                {
+                    {"songId", 10001},
+                    {"singerId", 1001},
+                    {"songName", "青花瓷"},
+                    {"duration", 239}
+                },
+                new Dictionary<string, dynamic>
+                {
+                    {"songId", 10002},
+                    {"singerId", 1001},
+                    {"songName", "发如雪"},
+                    {"duration", 253}
+                },
+                new Dictionary<string, dynamic>
+                {
+                    {"songId", 10003},
+                    {"singerId", 1001},
+                    {"songName", "东风破"},
+                    {"duration", 215}
+                },
+                new Dictionary<string, dynamic>
+                {
+                    {"songId", 1004},
+                    {"singerId", 3002},
+                    {"songName", "不为谁而作的歌"},
+                    {"duration", 296}
+                },
+                new Dictionary<string, dynamic>
+                {
+                    {"songId", 1005},
+                    {"singerId", 1002},
+                    {"songName", "背对背拥抱"},
+                    {"duration", 262}
+                }
+            };
+
+
+            //singerList专辑
+            //songList歌曲
+            //2、通过歌曲查找歌手
+            //Console.WriteLine("输入歌曲名称：");
+            //string song = Console.ReadLine();
+
+            //foreach (var songItem in songList)
+            //{
+            //    if (songItem["songName"].ToString().Equals(song, StringComparison.OrdinalIgnoreCase))
+            //    {
+            //        int singerId = songItem["singerId"];
+            //        var singer = singerList.Find(s => s["singerId"] == singerId);
+            //        if (singer != null)
+            //        {
+            //            Console.WriteLine($"歌曲《{song}》的歌手是：{singer["singerName"]}");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine($"未找到歌曲《{song}》对应的歌手信息。");
+            //        }
+            //        return;
+            //    }
+            //}
+
+            //2、通过歌曲查找歌手 整活, 别用
+            ////songList.Find(s => s["songName"].ToString().Equals(Console.ReadLine(), StringComparison.OrdinalIgnoreCase));
+            //Console.WriteLine("输入歌曲名称：");
+            //string song = Console.ReadLine();
+            //var singer = singerList
+            //            .Find(s => s["singerId"] == songList.Find(s => s["songName"]
+            //                .ToString()
+            //                .Equals(song, StringComparison.OrdinalIgnoreCase))["singerId"]
+            //            );
+            //Console.WriteLine(singer["singerName"]);
         }
     }
 }
