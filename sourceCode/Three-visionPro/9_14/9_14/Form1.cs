73bac77132639c2ae0effc1dafb4d35b290bac3e
雪 <242847242@qq.com>
2026-09-15 08:02:19 +0800

    #9_15

---
+++ sourceCode/Three-visionPro/9_14/9_14/Form1.cs
@@ -0,0 +1,49 @@
+﻿using Cognex.VisionPro;
+using Cognex.VisionPro.ImageFile;
+using Cognex.VisionPro.ToolBlock;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9_14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog OFD = new OpenFileDialog()) {
+                OFD.Filter = "图片格式|*.jpg;jpeg;*png;*.gif;*.bmp;";
+                OFD.Title = "选择硬币图片";
+
+                if (OFD.ShowDialog() == DialogResult.OK) {
+                    pictureBox1.Image = Image.FromFile(OFD.FileName);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                    CogImageFileTool CIFT = new CogImageFileTool();
+                    CIFT.Operator.Open(OFD.FileName, CogImageFileModeConstants.Read);
+                    CIFT.Run();
+
+                    string vppFilePath = Path.Combine(Directory.GetCurrentDirectory(), "vpps", "number.vpp");
+                    object CSL = CogSerializer.LoadObjectFromFile(vppFilePath);
+                    CogToolBlock CTB = (CogToolBlock)CSL;
+                    CTB.Inputs["OutputImage"].Value = CIFT.OutputImage;
+
+                    CTB.Run();
+                    label1.Text = CTB.Outputs["Output"].Value.ToString();
+                }
+            }
+        }
+    }
+}
